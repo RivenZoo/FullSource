a48b7ff75d205cfed7757ae894545abab2772c23
Dark.Hades <dark.hades.1102@gmail.com>
2018-07-23 19:29:35 +0900

    Declare type

--- Jx3Full/Include/KG3DEngine/KG3DTypes.h
+++ Jx3Full/Include/KG3DEngine/KG3DTypes.h
@@ -1,4 +1,24 @@
 #ifndef _KG3DTYPES_H_
 #define _KG3DTYPES_H_
 
+#include <functional>
+#include <map>
+#include <string>
+
+using TagSFXCallBackExtraInfo = std::map<std::string, std::function<void()>>;
+enum 
+{
+	MTBT_DAMAGEPERCENTAGE,
+	MTBT_MISSILE,
+};
+struct TagMotionCallBackExtraInfo
+{
+	void fData;
+	DWORD dwData;
+};
+
+enum class enuAnimationControllerPriority
+{
+};
+
 #endif
